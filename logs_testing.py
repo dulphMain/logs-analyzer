f26377fcca000cd02d98ec6935c2bd5e9e07a0b8
dulphMain <augustin.michau@outlook.com>
2026-07-07 11:21:48 +0200

    Added new error diagnosis
    Adds multiple new errors and fixes some bugs/incorrect regexes

--- logs_testing.py
+++ logs_testing.py
@@ -3,7 +3,7 @@
 @version: 1.0.0
 """
 import regex
-from_url = False #Default mode, requires only regex module
+from_url = False #Default mode is False, requiring only regex module
 
 class FetchingError(Exception):
 	pass
@@ -81,7 +81,7 @@ output="""It has been detected that the log has an incompatible mod version of `
 >   Either use the app or this [file link](https://www.curseforge.com/minecraft/mc-mods/corgilib/files/5436749)."""
 print(search_content(re, content, output), end="")
 
-#Problem 3 (corrupted config files)
+#Problem 3 (corrupted config files) #obsolete
 re=r"com\.electronwill\.nightconfig\.core\.io\.ParsingException: Not enough data available"
 output="""This issue must be dealt with in order for the profile to successfully load.
 
@@ -97,12 +97,19 @@ print(search_content(re, content, output), end="")
 
 #Problem 3 diagnosis 2 (corrupted config files -> detailed)
 re=r"net\.minecraftforge\.fml\.config\.ConfigFileTypeHandler\$ConfigLoadingException: Failed loading config file ([a-zA-Z0-9_\.-]+) of type ([A-Z]+) for modid ([a-zA-Z_-]+)"
-output="Corrupted config file found : please delete file `%GROUP1%` (type %GROUP2%)."
+output="""The configuration file `%GROUP1%` is corrupted. This often happens due to extreme crashes such as JVM errors or bluescreens.
+
+The file must be removed to fix the profile.
+**Options to remove corrupted config files:**
+- Find the file mentioned above (probably inside the profiles `config` folder, however `server` type configs are sometimes in `saves/worldname/serverconfig` instead) and delete it.
+- Get the community created [helper script from this Curseforge community github site](https://github.com/CurseForgeCommunity/Script-Tools) and run it to scan for, and remove corrupted config files.  
+    - Place a copy of the script file in the profile folder you would like to repair.  To find the profile folder - Enter the profile screen in the Curseforge app, go to **options** <:zCFmenu:1319318631770751086> and then select **open folder**
+    - Run the file which you have copied to the profile folder, wait for it to finish scanning - and confirm if you would like to remove what it found."""
 print(search_content(re, content, output), end="")
 
 #Problem 4 (red herring errors)
 re="Cowardly refusing to send event"
-output="Only errors before the first `Cowardly refusing to send event` are the cause!"
+output="Look for the error before `Cowardly refusing to send event`"
 print(search_content(re, content, output), end="")
 
 #Problem 5 (duplicate mods)
@@ -135,13 +142,13 @@ output="Possible Mac display issues. Run command !mc-mac and follow the instruct
 print(search_content(re, content, output), end="")
 
 #Problem 9.1 (MixinExtras conflict)
-re=r"module (mixinextras\.neoforge|MixinExtras) exports package [a-zA-Z0-9_.-]* to (mixinextras\.neoforge|MixinExtras)"
+re=r"module (mixinextras\.neoforge|MixinExtras) exports package [a-zA-Z0-9_.-]* to (mixinextras\.neoforge|MixinExtras)|Modules mixinextras\.neoforge and MixinExtras|Modules MixinExtras and mixinextras\.neoforge"
 output="""Your log file indicates that you have two or more mods including conflicting versions of MixinExtras.
 To fix this you can install this mod: https://www.curseforge.com/minecraft/mc-mods/mixin-extras-neoforge-on-forge-fix"""
 print(search_content(re, content, output), end="")
 
-#Problem 9.2 (MixinExtras wrong modloader)
-re=r"Module (?:mixinextras.neoforge|MixinExtras) contains package [a-zA-Z0-9_.-]+, module (?:mixinextras.neoforge|MixinExtras) exports package [a-zA-Z0-9_.-]+ to (?:mixinextras.neoforge|MixinExtras)"
+#Problem 9.2 (MixinExtras incorrect inclusion)
+re=r"Module (?:mixinextras.neoforge|MixinExtras) contains package [a-zA-Z0-9_.-]+, module (?:mixinextras.neoforge|MixinExtras) exports package [a-zA-Z0-9_.-]+ to (?:mixinextras.neoforge|MixinExtras)|Modules MixinExtras and mixinextras\.neoforge export package|Modules mixinextras\.neoforge and MixinExtras export package"
 output="""You have mods including a NeoForge specific version of MixinExtras on Forge.
 
 Install [Mixin Extras NeoForge on Forge Fix](https://www.curseforge.com/minecraft/mc-mods/mixin-extras-neoforge-on-forge-fix)
@@ -168,7 +175,7 @@ re="maximum id range exceeded"
 output="Your log looks to have a problem with the maximum number of IDs for something being already taken. To increase the limit to modern Minecraft's amounts, run the `!mc-idfix` command and follow the instructions"
 print(search_content(re, content, output), end="")
 
-#Problem 13 (Unsupported mods in profile)
+#Problem 13 (Unsupported mods in profile) #obsolete
 re=r"java\.lang\.UnsupportedClassVersionError"
 output="""- Your log contains at least one error message reporting a mod file which is made (compiled) for a different Java version.  
 - This means that the mod file(s) are made for a different Minecraft version also.  
@@ -183,13 +190,20 @@ Class version 65 - MC 1.20.5 and newer`"""
 print(search_content(re, content, output), end="")
 
 #Problem 13 version 2 (Unsupported mods in profile -> advanced)
-re=r"java\.lang\.UnsupportedClassVersionError: ([a-zA-Z0-9_\.-]+(\/[a-zA-Z0-9_\.-]+)*) has been compiled by a more recent version of the Java Runtime \(class file version ([0-9]*\.[0-9]*)\), this version of the Java Runtime only recognizes class file versions up to ([0-9]*\.[0-9]*)"
-output="""- Your log contains at least one error message reporting a mod file which is made (compiled) for a different Java version.  
-- This means that the mod file(s) are made for a different Minecraft version also.  
-- You will need to either remove the mod(s) entirely, or replace with a file for the same mod project but compatible with the same Minecraft version of the profile.  
-[List of **class file versions** vs **Java versions**](https://javaalmanac.io/bytecode/versions/)
+re=r"java\.lang\.UnsupportedClassVersionError(: ([a-zA-Z0-9_\.-]+(\/[a-zA-Z0-9_\.-]+)*) has been compiled by a more recent version of the Java Runtime \(class file version ([0-9]*\.[0-9]*)\), this version of the Java Runtime only recognizes class file versions up to ([0-9]*\.[0-9]*))?"
+output="""You have a mod that is made for a different Java version, which indicates it is for a different Minecraft version.
+The mod name is contained in the following path: `%GROUP2%`.
 
-The mod name is contained in the following path: `%GROUP1%`. It has been compiled to version `%GROUP3%` but this version of the Java Runtime only recognizes class file versions up to `%GROUP4%`."""
+It has been compiled to Java Class version `%GROUP4%` but this Java Class version only supports `%GROUP5%`.
+
+To fix this you should either remove the mod or find a version that is compatible, using the following list of versions
+
+`Class version 52 - MC 1.16.5 and older
+Class version 61 - MC 1.18 to 1.20.4
+Class version 65 - MC 1.20.5 and newer
+Class version 69 - MC 26.1 and newer`
+
+In case this list does not contain a version, [Class to Java versions can be found here](https://javaalmanac.io/bytecode/versions/) and [Java to Minecraft versions here](https://minecraft.wiki/w/Tutorial:Update_Java#Why_update?)"""
 print(search_content(re, content, output), end="")
 
 #Problem 14 (MC launching with wrong videocard)
@@ -202,7 +216,7 @@ This means that the game requires a dedicated graphics card be used with the gam
 !mc-videocard"""
 print(search_content(re, content, output), end="")
 
-#Problem 15 (NBT too big)
+#Problem 15 (NBT too long)
 re=r"(Tried to read NBT tag with too high complexity)|(Tried to read NBT tag that was too big)"
 output="""For one reason or another you have objects in your world with NBT data with a depth greater than 512. For example, a backpack with too many items or too much NBT data.
 You can add this mod to try and get around this limitation.  
@@ -212,7 +226,7 @@ You can add this mod to try and get around this limitation.
 print(search_content(re, content, output), end="")
 
 #Problem 16 (DH and Oculus/Iris not compatible)
-re=r"(ClassMetadataNotFoundException: com.seibel.distanthorizons.coreapi.util.math.Mat4f)|(MixinApplyError: Mixin \[mixins.oculus.compat.dh.json:NonCullingFrustumMixin\])"
+re=r"ClassMetadataNotFoundException: com\.seibel\.distanthorizons\.coreapi\.util\.math\.Mat4f|MixinApplyError: Mixin \[mixins\.oculus\.compat\.dh\.json:NonCullingFrustumMixin\]"
 output="Versions of mods Distant Horizons and Oculus/Iris not compatible. Run command `!mc-dh` to see how to fix it."
 print(search_content(re, content, output), end="")
 
@@ -220,6 +234,33 @@ print(search_content(re, content, output), end="")
 re="pojavlaunch"
 output="We do not provide support for that launcher here, please reach out the the PojavLauncher devs for support"
 print(search_content(re, content, output), end="")
+
+#Problem 18 (important tag failing to load)
+re=r"Couldn't load tag (minecraft:music_discs|minecraft:mineable/pickaxe|minecraft:mineable/axe|minecraft:mineable/shovel|minecraft:mineable/hoe|minecraft:needs_diamond_tool|minecraft:needs_iron_tool|minecraft:needs_stone_tool|minecraft:incorrect_for_wooden_tool|minecraft:incorrect_for_stone_tool|minecraft:incorrect_for_netherite_tool|minecraft:incorrect_for_iron_tool|minecraft:incorrect_for_gold_tool|minecraft:incorrect_for_diamond_tool|minecraft:enchantable/[a-z0-9_/]+|minecraft:[a-z0-9_]+_spawnable_on|minecraft:logs|minecraft:dolphin_located|minecraft:cats_spawn_in|minecraft:eye_of_ender_located|minecraft:infiniburn_end|minecraft:infiniburn_nether|neoforge:enchanting_fuels|neoforge:piglin_usable_crossbows|neoforge:pillager_usable_crossbows|neoforge:skeleton_usable_bows|neoforge:needs_netherite_tool|neoforge:villager_farmlands) as it is missing following references: ((?:\n\t)?(?:#?[a-z0-9_.-]+:[a-z0-9/_.-]+) \(from .*?\)(?:, )?)+"
+output="""The tag `%GROUP1%` has failed to load. This tag controls important gameplay behaviour, so you should fix this.
+
+The tag failed to load because of some missing entries due to straight up broken mods. You need to remove these mods:```%GROUP2%```
+If these mods are absolutely required you can report the issue to the mod author then install [LMFT](https://www.curseforge.com/minecraft/mc-mods/lmft)."""
+print(search_content(re, content, output), end="")
+
+#Problem 19 (Legacy random source error debugging)
+re="java.lang.IllegalStateException: Accessing LegacyRandomSource from multiple threads"
+ouptut="""Some mod is accessing a single threaded resource from multiple threads, which breaks the game.
+
+Please install https://www.curseforge.com/minecraft/mc-mods/uwrad-forge then cause the issue again and resend logs to make this easier to diagnose."""
+print(search_content(re, content, output), end="")
+
+#Problem 20 (malformed options.txt)
+re=r"Duplicate key ([a-zA-Z0-9_.-]+) \(attempted merging values ([a-zA-Z0-9_.-]+) and ([a-zA-Z0-9_.-]+)\)"
+output="""Your option.txt file has some duplicate entries, which causes (Neo)Forge to fail to load it.
+
+The easiest way to fix this is to delete/rename your options.txt file. You could also manually edit it, removing one of these two lines:
+```
+%GROUP1%=%GROUP2%
+%GROUP1%=%GROUP3%
+```"""
+print(search_content(re, content, output), end="")
+
 
 if not from_url:
 	opened.close()
